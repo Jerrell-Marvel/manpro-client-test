fe56157a9feae2caba0525ad78fe486b43656b15
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-23 16:30:59 +0700

    integrate ui with register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -21,6 +21,7 @@ export default function Register() {
   const [selectedKecamatan, setSelectedKecamatan] = useState<number>();
   const [selectedKelurahan, setSelectedKelurahan] = useState<number>();
 
+  const [nama, setNama] = useState<string>("");
   const [noHp, setNoHp] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [alamat, setAlamat] = useState<string>("");
@@ -75,93 +76,261 @@ export default function Register() {
       email,
       kelId: selectedKelurahan,
       password,
+      nama,
     });
   };
 
   return (
-    <div>
-      <button onClick={() => console.log(selectedKecamatan)}>click kec</button>
-      <button onClick={() => console.log(selectedKelurahan)}>click kel</button>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="text"
-          name="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
+    <>
+      <div>
+        <button onClick={() => console.log(selectedKecamatan)}>click kec</button>
+        <button onClick={() => console.log(selectedKelurahan)}>click kel</button>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
 
-        <label htmlFor="no-hp">No HP</label>
-        <input
-          id="no-hp"
-          type="text"
-          name="noHp"
-          onChange={(e) => setNoHp(e.target.value)}
-          value={noHp}
-        />
+          <label htmlFor="no-hp">No HP</label>
+          <input
+            id="no-hp"
+            type="text"
+            name="noHp"
+            onChange={(e) => setNoHp(e.target.value)}
+            value={noHp}
+          />
 
-        <label htmlFor="alamat">Alamat</label>
-        <input
-          id="alamat"
-          type="text"
-          name="alamat"
-          onChange={(e) => setAlamat(e.target.value)}
-          value={alamat}
-        />
+          <label htmlFor="alamat">Alamat</label>
+          <input
+            id="alamat"
+            type="text"
+            name="alamat"
+            onChange={(e) => setAlamat(e.target.value)}
+            value={alamat}
+          />
 
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          name="noHp"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="noHp"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
 
-        <label htmlFor="kecamatan">Kecamatan</label>
-        <select
-          name="kecamatan"
-          id="kecamatan"
-          onChange={(e) => {
-            setSelectedKecamatan(Number(e.target.value));
-          }}
-        >
-          {kecamatan?.map((kec) => {
-            return (
-              <option
-                value={kec.kec_id}
-                key={kec.kec_id}
+          <label htmlFor="kecamatan">Kecamatan</label>
+          <select
+            name="kecamatan"
+            id="kecamatan"
+            onChange={(e) => {
+              setSelectedKecamatan(Number(e.target.value));
+            }}
+          >
+            {kecamatan?.map((kec) => {
+              return (
+                <option
+                  value={kec.kec_id}
+                  key={kec.kec_id}
+                >
+                  {kec.nama_kec}
+                </option>
+              );
+            })}
+          </select>
+
+          <label htmlFor="kelurahan">kelurahan</label>
+          <select
+            name="kelurahan"
+            id="kelurahan"
+            value={selectedKelurahan}
+            onChange={(e) => setSelectedKelurahan(Number(e.target.value))}
+          >
+            {kelurahan?.map((kel) => {
+              return (
+                <option
+                  value={kel.kel_id}
+                  key={kel.kel_id}
+                >
+                  {kel.nama_kel}
+                </option>
+              );
+            })}
+          </select>
+
+          <input type="submit" />
+        </form>
+      </div>
+
+      <div className="bg-gray-100 flex items-center justify-center min-h-screen">
+        <div className="bg-white shadow-lg rounded-lg p-8 w-96 fade-in">
+          <h1 className="text-2xl font-bold text-center mb-4">Buat Akun</h1>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div className="mb-4">
+              <label
+                className="block text-sm font-semibold mb-2"
+                htmlFor="name"
               >
-                {kec.nama_kec}
-              </option>
-            );
-          })}
-        </select>
-
-        <label htmlFor="kelurahan">kelurahan</label>
-        <select
-          name="kelurahan"
-          id="kelurahan"
-          value={selectedKelurahan}
-          onChange={(e) => setSelectedKelurahan(Number(e.target.value))}
-        >
-          {kelurahan?.map((kel) => {
-            return (
-              <option
-                value={kel.kel_id}
-                key={kel.kel_id}
+                Nama Lengkap
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Masukkan nama"
+                required
+                className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-300"
+                onChange={(e) => {
+                  setNama(e.target.value);
+                }}
+                value={nama}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-sm font-semibold mb-2"
+                htmlFor="email"
               >
-                {kel.nama_kel}
-              </option>
-            );
-          })}
-        </select>
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                required
+                className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-300"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-sm font-semibold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                required
+                className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-300"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-sm font-semibold mb-2"
+                htmlFor="no-telp"
+              >
+                Nomor Telepon
+              </label>
+              <input
+                type="text"
+                id="no-telp"
+                placeholder="Masukkan nomor telepon"
+                required
+                className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-300"
+                onChange={(e) => setNoHp(e.target.value)}
+                value={noHp}
+              />
+            </div>
 
-        <input type="submit" />
-      </form>
-    </div>
+            <div className="mb-4">
+              {/*  */}
+              <div>
+                <label
+                  htmlFor="kecamatan"
+                  className="text-sm font-semibold mb-2"
+                >
+                  Kecamatan :{" "}
+                </label>
+                <select
+                  name="kecamatan"
+                  id="kecamatan"
+                  onChange={(e) => {
+                    setSelectedKecamatan(Number(e.target.value));
+                  }}
+                >
+                  {kecamatan?.map((kec) => {
+                    return (
+                      <option
+                        value={kec.kec_id}
+                        key={kec.kec_id}
+                      >
+                        {kec.nama_kec}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="kelurahan"
+                  className="text-sm font-semibold mb-2"
+                >
+                  kelurahan :{" "}
+                </label>
+                <select
+                  name="kelurahan"
+                  id="kelurahan"
+                  value={selectedKelurahan}
+                  onChange={(e) => setSelectedKelurahan(Number(e.target.value))}
+                >
+                  {kelurahan?.map((kel) => {
+                    return (
+                      <option
+                        value={kel.kel_id}
+                        key={kel.kel_id}
+                      >
+                        {kel.nama_kel}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+
+              {/*  */}
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-sm font-semibold mb-2"
+                htmlFor="alamat"
+              >
+                Alamat Lengkap
+              </label>
+              <input
+                type="text"
+                id="alamat"
+                name="alamat"
+                required
+                className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-300"
+                value={alamat}
+                onChange={(e) => {
+                  setAlamat(e.target.value);
+                }}
+                placeholder="Masukan alamat lengkap"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
